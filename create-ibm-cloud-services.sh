b754d67aed52fd0fdc097b8fe74aa08b6547d336
Manoj Jahgirdar <52746337+manojjahgirdar@users.noreply.github.com>
2021-06-06 13:04:27 +0530

    Update create-ibm-cloud-services.sh

--- create-ibm-cloud-services.sh
+++ create-ibm-cloud-services.sh
@@ -1,9 +1,9 @@
 servicename="cp-wa"
 service="conversation"
 region="eu-gb"
-ibmcloud resource service-instance-create $servicename $service lite $region
+ibmcloud resource service-instance-create $servicename $service free $region
 ibmcloud resource service-key-create "$servicename-creds" Manager --instance-name $servicename > "$servicename.txt"  2>&1
 apikey=$(cat $servicename.txt | awk '$1 == "apikey:" {print $2}')
 url=$(cat $servicename.txt | awk '$1 == "url:" {print $2}')
 JSON_STRING='{"apikey":"'"$apikey"'","url":"'"$url"'"}'
-echo $JSON_STRING > speechtotext.json
+echo $JSON_STRING > watsonassistant.json
