d9be43a2d1a45df7badb59889d342ea9e4c9db60
manojjahgirdar <manoj.jahgirdar@in.ibm.com>
2022-03-22 13:16:05 +0530

    Fixed NPM vulnerabilities

--- node-web-application/node_modules/ibm-watson/lib/common.d.ts
+++
@@ -1,5 +0,0 @@
-export declare type SdkHeaders = {
-    'User-Agent': string;
-    'X-IBMCloud-SDK-Analytics': string;
-};
-export declare function getSdkHeaders(serviceName: string, serviceVersion: string, operationId: string): SdkHeaders | {};
